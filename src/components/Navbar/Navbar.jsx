60b493aefde2dcef971399d3696b6ac643c1c3e9
amarell <mujagicamar@gmail.com>
2022-05-08 22:29:29 +0200

    Added onClickHandler for button component

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,17 +1,21 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import Button from "../Button";
 import styles from "./Navbar.module.scss";
 
 function Navbar() {
+  const navigate = useNavigate();
   return (
     <div>
       <div className={styles["navbar-container"]}>
-        <div className={styles.logo}>Logo</div>
+        <div className={styles.logo} onClick={() => navigate("/")}>
+          Logo
+        </div>
         <div className={styles.buttons}>
           <Button
             variant="contained"
-            className={styles["amarweajklsdfl"]}
             text="Login"
+            onClickHandler={() => navigate("/login")}
           />
           <Button variant="outlined" text="Register" />
         </div>
